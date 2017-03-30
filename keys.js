fbfbba3657e52b9db9380bf0794ee76e963e13b0
Jonathan Sarro <jon@Jonathans-MacBook-Pro.local>
2017-03-29 18:11:36 -0700

    added keys

---
+++ keys.js
@@ -0,0 +1,8 @@
+console.log('this is loaded');
+
+exports.twitterKeys = {
+  consumer_key: 'JwxEyGDEe3KRkM8vwZbZB2G0P',
+  consumer_secret: '0X0kWaxX4cEbs2jQJ8HpodTipTUaVm2rzOO2MNULUZ96qzNtln',
+  access_token_key: '3086980376-CWhKbz78Sa0pNv4aLJcckE02lTdEo1hloLw4uM9',
+  access_token_secret: 'BUcmXuuyAk5l10EZrR0G2T5qird7os94J9N1DuRKbTcY',
+}
